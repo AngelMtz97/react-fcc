8337dc0fb4271a807a0175e82ef72ff0e3a14edd
Angel Sangerman <armando.amartz97@gmail.com>
2024-01-21 18:23:31 -0600

    Create a Complex JSX Element

---
+++ solution2.jsx
@@ -0,0 +1,11 @@
+const JSX = (
+  <div>
+     <h1>Hello</h1>
+     <p>This is a paragraph</p>
+     <ul>
+      <li>Item one</li>
+      <li>Item two</li>
+      <li>Item three</li>
+     </ul>
+  </div>
+)
